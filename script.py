6a0a9b3f8b6e005f42eb1444f41deacc4397496a
kobesar <klobby19@gmail.com>
2023-02-20 22:53:14 -0800

    readme

--- script.py
+++ script.py
@@ -18,13 +18,14 @@ for team in full_league:
   for player in team_df['PLAYER_ID']:
     pid = player
     player_df = commonplayerinfo.CommonPlayerInfo(player_id=pid).get_data_frames()[0]
-    pid = player_df['PERSON_ID'][0]
-    pname = player_df['DISPLAY_FIRST_LAST'][0]
-    player_pick = player_df['DRAFT_NUMBER'][0]
-    lottery = int(player_pick) <= 14 if player_pick != 'Undrafted' else False
-    lottery_prop += 1 if lottery is True else 0 
-    # team_res['players'].append({'pid': pid, 'pname': pname, 'pick': player_pick, 'lottery': lottery})
-    time.sleep(1)
+    if len(player_df) > 0:
+      pid = player_df['PERSON_ID'][0]
+      pname = player_df['DISPLAY_FIRST_LAST'][0]
+      player_pick = player_df['DRAFT_NUMBER'][0]
+      lottery = int(player_pick) <= 14 if player_pick != 'Undrafted' and player_pick is not None else False
+      lottery_prop += 1 if lottery is True else 0 
+      # team_res['players'].append({'pid': pid, 'pname': pname, 'pick': player_pick, 'lottery': lottery})
+      time.sleep(1)
   sum_teams.append({'team': tname, 'lottery_prop': lottery_prop / len(team_df['PLAYER_ID'])})
   # all_teams.append(team_res)
 
